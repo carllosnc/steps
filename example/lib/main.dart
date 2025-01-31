dc98a24e962ad7b0ad3f64fcffd6ba8cab51244f
Carlos Costa <carllos.nc@gmail.com>
2025-01-31 17:04:59 -0300

    Refactor main app structure to use HomePage and add routes for ManualPage1 and ManualPage2

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:example/pages/home_page.dart';
+import 'package:example/pages/manual_page1.dart';
+import 'package:example/pages/manual_page2.dart';
 import 'package:flutter/material.dart';
-import 'package:steps/steps.dart';
 
 void main() {
   runApp(const MainApp());
@@ -12,139 +14,11 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: WelcomeSteps(
-            backgroundColor: Colors.blueAccent,
-            screens: [
-              WelcomeScreen(
-                backgroundColor: Colors.red.shade700,
-                content: const Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Image(
-                      image: AssetImage('lib/assets/01.png'),
-                      width: 300,
-                    ),
-                    SizedBox(height: 20),
-                    Text(
-                      'First Screen',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                        fontSize: 25,
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    Text(
-                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown.",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 14,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              WelcomeScreen(
-                backgroundColor: Colors.orange.shade700,
-                content: const Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Image(
-                      image: AssetImage('lib/assets/02.png'),
-                      width: 300,
-                    ),
-                    SizedBox(height: 20),
-                    Text(
-                      'Second Screen',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                        fontSize: 25,
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    Text(
-                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown.",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 14,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              WelcomeScreen(
-                backgroundColor: Colors.blue.shade700,
-                content: const Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Image(
-                      image: AssetImage('lib/assets/03.png'),
-                      width: 300,
-                    ),
-                    SizedBox(height: 20),
-                    Text(
-                      'Third Screen',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                        fontSize: 25,
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    Text(
-                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown.",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 14,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              WelcomeScreen(
-                backgroundColor: Colors.deepPurple.shade700,
-                content: const Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Image(
-                      image: AssetImage('lib/assets/04.png'),
-                      width: 300,
-                    ),
-                    SizedBox(height: 20),
-                    Text(
-                      'Fourth Screen',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                        fontSize: 25,
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    Text(
-                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown.",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 14,
-                      ),
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
+      home: const HomePage(),
+      routes: {
+        "manual1": (context) => const ManualPage1(),
+        "manual2": (context) => const ManualPage2(),
+      },
     );
   }
 }
